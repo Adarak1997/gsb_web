7328eaf7dbd49a06837a8adbc5aab7612b7c4e29
Adarak <sydneymurat@yahoo.fr>
2019-06-01 20:19:07 +0200

    formulaire frais et hors forfait fini

--- visiteur/compte_visiteur.php
+++ visiteur/compte_visiteur.php
@@ -140,7 +140,7 @@ $reponse=$bdd->query("SELECT details_frais_forfait.id, details_frais_forfait.qua
                 <th scope="col">Etat</th>
               </tr>
               <?php  while($row = $reponse->fetch()){
-              echo "<tr><td>". $row["libelle"]."</td><td>". $row["quantite"]. "</td><td>". $row["montant"]*$row["quantite"]. "</td><td>".  $row["libel"].  "<td></tr>";
+              echo "<tr><td>". $row["libelle"]."</td><td>". $row["quantite"]. "</td><td>". $row["montant"]*$row["quantite"]. "</td><td>".  $row["libel"].  "</td></tr>";
   
               }
               echo "</table>";
@@ -162,6 +162,8 @@ $reponse=$bdd->query("SELECT details_frais_forfait.id, details_frais_forfait.qua
             WHERE fiche_frais.mois = '".$mois."'
       AND fiche_frais.annee = '".$annee."'
       AND utilisateur_id = '".$userId."'");
+
+
   
   
             ?>
@@ -182,7 +184,7 @@ $reponse=$bdd->query("SELECT details_frais_forfait.id, details_frais_forfait.qua
   
         
   
-        echo "<tr><td>". $row["libelle"]."</td><td>". $row["montant"]. "</td><td>".  $row["libelle_etat"].  "<td></tr>";
+        echo "<tr><td>". $row["libelle"]. "</td><td>". $row["montant"]. "</td><td>". $row["libelle_etat"]. "<td></tr>";
   
       }
       echo "</table>";
@@ -192,6 +194,19 @@ $reponse=$bdd->query("SELECT details_frais_forfait.id, details_frais_forfait.qua
           </div>
       <br><br>
   
+
+      <?php
+$reponse= $bdd->query("SELECT fiche_frais.id, fiche_frais.mois, fiche_frais.annee, 
+fiche_frais.utilisateur_id as utilisateur_id
+FROM fiche_frais 
+WHERE (fiche_frais.mois = '".$mois."' AND fiche_frais.annee = '".$annee."') 
+AND utilisateur_id = '".$userId."'");
+
+$sql = $reponse->fetch();
+$idFiche = $sql['id'];
+
+?>
+      
   
           
         </div>
@@ -201,28 +216,33 @@ $reponse=$bdd->query("SELECT details_frais_forfait.id, details_frais_forfait.qua
             <header style="margin-bottom:20px;" class="w3-container w3-blue">
               <h1>Ajout frais forfaitisés</h1>
             </header>
-            <form style="margin-bottom:20px;" class="w3-container" action="../fonction/newFraisForfait.php" method="POST">  
+            <form style="margin-bottom:20px;" class="w3-container" action="" method="POST">                                        
                 <div class="form-group">
-                  <label style="float:left;">Choix du frais forfait</label>
-                  <select name="frais_forfait" class="form-control">
-                    <option selected>Sélectionner un frais forfait</option>
-                    <?php  foreach ($fraiss as $frais) { ?>
-                    <option value="<?php echo $frais['id']; ?>"><?php echo $frais['libelle']; ?></option>
-                    <?php } ?>
-                  </select>
-                </div> 
-                        
+                  <label style="float:left;">Kilomètres</label>
+                  <input type="number" min="0"  name="kilometre" class="form-control" placeholder="Rentrez le nombre de kilomètres parcourus">
+                </div>
                 <div class="form-group">
-                  <label style="float:left;">Quantité du frais</label>
-                  <input type="number" min="0"  name="quantite" class="form-control" placeholder="Rentrez votre quantité" required>
+                  <label style="float:left;">Nuits</label>
+                  <input type="number" min="0"  name="nuit" class="form-control" placeholder="Rentrez le nombre de nuits">
+                </div>
+                <div class="form-group">
+                  <label style="float:left;">Repas midi</label>
+                  <input type="number" min="0"  name="repas_midi" class="form-control" placeholder="Rentrez le nombre de repas midi">
+                </div>
+                <div class="form-group">
+                  <label style="float:left;">Relais étape</label>
+                  <input type="number" min="0"  name="relais_etape" class="form-control" placeholder="Rentrez le nombre de relais étape">
                 </div>
                 <input style="padding: 10px 80px;" class="btn btn-primary btnAjout" type="submit" value="Valider"/>
               </form>
 
+        
+
+
             <header style="margin-bottom:20px;" class="w3-container w3-blue">
               <h1>Ajout frais non forfaitisés</h1>
             </header>
-            <form style="padding-bottom:20px;" class="w3-container" action="../fonction/newFraisForfait.php" method="POST">  
+            <form style="padding-bottom:20px;" class="w3-container" method="POST" action="">  
                 <div class="form-group">
                   <label style="float:left;">Libellé frais</label>
                   <input name="libelle" class="form-control" placeholder="Rentrez le libellé" required>
@@ -232,12 +252,151 @@ $reponse=$bdd->query("SELECT details_frais_forfait.id, details_frais_forfait.qua
                   <label style="float:left;">Montant du frais</label>
                   <input type="number" min="0"  name="montant" class="form-control" placeholder="Rentrez le montant" required>
                 </div>
-                <input style="padding: 10px 80px;" class="btn btn-primary btnAjout" type="submit" value="Valider"/>
+                <input style="padding: 10px 80px;" class="btn btn-primary btnAjout" type="submit" name="submit" value="Valider"/>
             </form>
           </div>
       </div>
     </div>
   </div>
+
+  <?php
+
+
+  if(isset($_POST['libelle']) && isset($_POST['montant'])){
+    
+
+    $fiche = $bdd->query("SELECT * FROM fiche_frais WHERE mois = '".$mois."' AND annee ='".$annee."' AND utilisateur_id ='".$userId."'");
+
+    if($fiche->rowCount()<1){
+      $libelle = $_POST['libelle'];
+    $montant = $_POST['montant'];
+      $creer=$bdd->prepare('INSERT INTO `fiche_frais` (`id`, `mois`, `annee`, `etat_id`, `utilisateur_id`)
+      VALUES (id, "'.$mois.'", "'.$annee.'", "1", "'.$userId.'")');
+
+      $creer->execute();
+
+    $FraisHorsForfait = $bdd->prepare('INSERT INTO `details_frais_non_forfait` (`id`, `libelle`, `montant`, `fiche_frais_id`, `etat_id`)
+    VALUES (id, "'.$libelle.'", "'.$montant.'" , "'.$idFiche.'" , "1")');
+    
+    
+    $FraisHorsForfait->execute();
+    
+    echo "<meta http-equiv='refresh' content=\"0; URL=compte_visiteur.php\">";
+  }else{
+    $libelle = $_POST['libelle'];
+    $montant = $_POST['montant'];
+    $FraisHorsForfait = $bdd->prepare('INSERT INTO `details_frais_non_forfait` (`id`, `libelle`, `montant`, `fiche_frais_id`, `etat_id`)
+    VALUES (id, "'.$libelle.'", "'.$montant.'" , "'.$idFiche.'" , "1")');
+    
+    
+    $FraisHorsForfait->execute();
+
+    echo "<meta http-equiv='refresh' content=\"0; URL=compte_visiteur.php\">";
+  }
+}
+
+  
+
+  if(isset($_POST['kilometre'])){
+    $kilometre = $_POST['kilometre'];
+
+    $verifier = $bdd->query("SELECT * FROM details_frais_forfait WHERE frais_forfait_id='1' AND fiche_frais_id='".$idFiche."'");
+    
+
+    if($verifier->rowCount()<1){
+      $FraisKilometre = $bdd->prepare('INSERT INTO `details_frais_forfait` (`id`, `quantite`, `frais_forfait_id`, `fiche_frais_id`, `etat_id`)
+    VALUES (id, "'.$kilometre.'", "1", "'.$idFiche.'", "1")');
+
+    $FraisKilometre->execute();
+
+    echo "<meta http-equiv='refresh' content=\"0; URL=compte_visiteur.php\">";
+    }else{
+      $updateKilometre = $bdd->prepare("UPDATE details_frais_forfait SET quantite = quantite + :quantite, frais_forfait_id ='1', fiche_frais_id = :fiche, etat_id ='1' WHERE frais_forfait_id ='1' AND fiche_frais_id ='".$idFiche."'");
+      $updateKilometre->bindparam(':quantite',$kilometre);
+      $updateKilometre->bindparam(':fiche',$idFiche);
+      $updateKilometre->execute();
+
+      echo "<meta http-equiv='refresh' content=\"0; URL=compte_visiteur.php\">";
+    }
+
+  }
+
+
+  if(isset($_POST['nuit'])){
+    $nuit = $_POST['nuit'];
+
+    $verifier2 = $bdd->query("SELECT * FROM details_frais_forfait WHERE frais_forfait_id='4' AND fiche_frais_id='".$idFiche."'");
+    
+
+    if($verifier2->rowCount()<1){
+      $FraisNuit = $bdd->prepare('INSERT INTO `details_frais_forfait` (`id`, `quantite`, `frais_forfait_id`, `fiche_frais_id`, `etat_id`)
+    VALUES (id, "'.$nuit.'", "4", "'.$idFiche.'", "1")');
+
+    $FraisNuit->execute();
+
+    echo "<meta http-equiv='refresh' content=\"0; URL=compte_visiteur.php\">";
+    }else{
+      $updateNuit = $bdd->prepare("UPDATE details_frais_forfait SET quantite = quantite + :quantite, frais_forfait_id ='4', fiche_frais_id = :fiche, etat_id ='1' WHERE frais_forfait_id ='4' AND fiche_frais_id ='".$idFiche."'");
+      $updateNuit->bindparam(':quantite',$nuit);
+      $updateNuit->bindparam(':fiche',$idFiche);
+      $updateNuit->execute();
+
+      echo "<meta http-equiv='refresh' content=\"0; URL=compte_visiteur.php\">";
+    }
+
+  }
+
+
+  if(isset($_POST['repas_midi'])){
+    $midi = $_POST['repas_midi'];
+
+    $verifier3 = $bdd->query("SELECT * FROM details_frais_forfait WHERE frais_forfait_id='2' AND fiche_frais_id='".$idFiche."'");
+    
+
+    if($verifier3->rowCount()<1){
+      $FraisMidi = $bdd->prepare('INSERT INTO `details_frais_forfait` (`id`, `quantite`, `frais_forfait_id`, `fiche_frais_id`, `etat_id`)
+    VALUES (id, "'.$midi.'", "2", "'.$idFiche.'", "1")');
+
+    $FraisMidi->execute();
+
+    echo "<meta http-equiv='refresh' content=\"0; URL=compte_visiteur.php\">";
+    }else{
+      $updateMidi = $bdd->prepare("UPDATE details_frais_forfait SET quantite = quantite + :quantite, frais_forfait_id ='2', fiche_frais_id = :fiche, etat_id ='1' WHERE frais_forfait_id ='2' AND fiche_frais_id ='".$idFiche."'");
+      $updateMidi->bindparam(':quantite',$midi);
+      $updateMidi->bindparam(':fiche',$idFiche);
+      $updateMidi->execute();
+
+      echo "<meta http-equiv='refresh' content=\"0; URL=compte_visiteur.php\">";
+    }
+
+  }
+  
+  if(isset($_POST['relais_etape'])){
+    $relais = $_POST['relais_etape'];
+
+    $verifier4 = $bdd->query("SELECT * FROM details_frais_forfait WHERE frais_forfait_id='3' AND fiche_frais_id='".$idFiche."'");
+    
+
+    if($verifier4->rowCount()<1){
+      $FraisRelais = $bdd->prepare('INSERT INTO `details_frais_forfait` (`id`, `quantite`, `frais_forfait_id`, `fiche_frais_id`, `etat_id`)
+    VALUES (id, "'.$relais.'", "3", "'.$idFiche.'", "1")');
+
+    $FraisRelais->execute();
+
+    echo "<meta http-equiv='refresh' content=\"0; URL=compte_visiteur.php\">";
+    }else{
+      $updateRelais = $bdd->prepare("UPDATE details_frais_forfait SET quantite = quantite + :quantite, frais_forfait_id ='3', fiche_frais_id = :fiche, etat_id ='1' WHERE frais_forfait_id ='3' AND fiche_frais_id ='".$idFiche."'");
+      $updateRelais->bindparam(':quantite',$relais);
+      $updateRelais->bindparam(':fiche',$idFiche);
+      $updateRelais->execute();
+
+      echo "<meta http-equiv='refresh' content=\"0; URL=compte_visiteur.php\">";
+    }
+
+  }
+  
+  ?>
+  
 
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>     
